112f6a5f5d81c05e5ce0477c27ee90183bf1a04b
marcos-mus1 <marcos.musafiri@zabibu.com>
2022-07-08 12:18:54 +0200

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { UsersComponent } from './components/users/users.component';
+import { LoginComponent } from './components/login/login.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NewContactComponent } from './components/new-contact/new-contact.component';
+import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
+import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    ContactsComponent,
+    UsersComponent,
+    LoginComponent,
+    SettingsComponent,
+    NewContactComponent,
+    ContactDetailsComponent,
+    EditContactComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
