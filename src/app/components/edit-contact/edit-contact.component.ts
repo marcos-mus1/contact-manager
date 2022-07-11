5ab8a17573ee4db0bb63034e95e649e51aa6968a
marcos-mus1 <marcos.musafiri@zabibu.co>
2022-07-11 18:51:30 +0200

    feat : add validation

--- src/app/components/edit-contact/edit-contact.component.ts
+++ src/app/components/edit-contact/edit-contact.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { IContact } from 'src/app/models';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-edit-contact',
   templateUrl: './edit-contact.component.html',
-  styleUrls: ['./edit-contact.component.css']
+  styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-
-  constructor() { }
+  contact!: IContact;
+  contactId!: number;
+  editForm!: FormGroup;
+  constructor(
+    private contactService: ContactService,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param) => {
+      this.contactId = Number(param.get('id'));
+    });
+    this.contactService.getContact(this.contactId).subscribe((contact) => {
+      this.contact = contact;
+      this.editForm = new FormGroup(
+        {
+          firstName: this.fb.control(contact.firstName, Validators.required),
+          lastName: this.fb.control(contact.lastName, Validators.required),
+          telephone: this.fb.control(contact.telephone, Validators.required),
+          email: this.fb.control(contact.email, Validators.required),
+          entreprise: this.fb.control(contact.entreprise, Validators.required),
+          address: this.fb.control(contact.address, Validators.required),
+          position: this.fb.control(contact.position, Validators.required),
+        },
+        { updateOn: 'submit' }
+      );
+    });
   }
 
+  onSubmit() {
+    if (this.editForm.valid) {
+      this.contactService.updateContat(this.contactId, this.editForm.value);
+    } else {
+      console.log('Invalid');
+      return;
+    }
+  }
 }
